c3163f4c4acdbf60d83a681392bc9cdfb7107d12
ines luna <inesluna19@gmail.com>
2019-11-14 17:53:51 +0100

    services sin testear

---
+++ src/services/auth-service.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+class Auth {
+  constructor() {
+    this.auth = axios.create({
+      baseURL: "http://localhost:4000",
+      withCredentials: true
+    });
+  }
+
+  signup(user) {
+    const { username, password,email, aboutMe } = user;
+    return this.auth
+      .post("/auth/signup", { username, password, email, aboutMe })
+      .then(({ data }) => data);
+  }
+
+  login(user) {
+    const { username, password } = user;
+    return this.auth
+      .post("/auth/login", { username, password })
+      .then(({ data }) => data);
+  }
+
+  logout() {
+    return this.auth.post("/auth/logout", {}).then( (response) => response.data);
+  }
+
+  me() {
+    return this.auth.get("/auth/me").then( (response) => response.data);
+  }
+
+  getUser(userId) {
+      return this.auth
+        .get(`/${userId}`)
+        .then((response) => response.data)
+  }
+
+  updateUser(user) {
+    return this.auth
+      .put("/auth/edit", user)
+      .then(( response ) => response.data);
+  }
+
+  deleteUser(user) {
+      return this.auth
+        .delete('/auth/delete', user)
+        .then((response) => response.data);
+  }
+}
+
+const authService = new Auth();
+
+export default authService;
---
+++ src/services/posts-service.js
@@ -0,0 +1,71 @@
+import axios from "axios";
+
+class Post {
+    constructor() {
+        this.post = axios.create({
+        baseURL: "http://localhost:4000",
+        withCredentials: true
+        });
+    }
+
+    getAllPosts(){
+        return this.post.get("/posts/").then( (response) => response.data);
+    }
+
+    getOnePost(postId){
+        return this.post.get(`/posts/${postId}`).then( (response) => response.data);
+    }
+
+    createPost(post) {
+        const { author, theme, city,country, textContent, makeThisHappend } = post;
+        return this.post
+          .post("/posts/create", { author, theme, city,country, textContent, makeThisHappend})
+          .then(({ data }) => data);
+    }
+
+    updatePost(post, postId) {
+    const { author, theme, city,country, textContent, makeThisHappend } = post;
+
+    return this.post
+        .put(`/${postId}/edit`, { author, theme, city,country, textContent, makeThisHappend})
+        .then(({ data }) => data);
+    }
+
+    deletePost(post) {
+        return this.post
+          .delete('/posts/delete', post)
+          .then(({data}) => data);
+    }
+
+    //COMMENTS ROUTES
+
+    getAllComments(postId){
+        return this.post
+        .get(`/posts/${postId}/comments`).then( (response) => response.data);
+    }
+
+    createComment(postId, comment) {
+        const { textContent } = comment;
+        return this.post
+          .post(`/posts/${postId}/create`, { textContent})
+          .then(({ data }) => data);
+    }
+
+    updateComment(postId, comment, commentId) {
+        const { textContent } = comment;
+        return this.post
+          .put(`/posts/${postId}/${commentId}/edit`, { textContent })
+          .then(({ data }) => data);
+    }
+
+    deleteComment(comment, commentId) {
+        return this.post
+          .put(`/posts/comment/${commentId}/delete`)
+          .then(({ data }) => data);
+    }
+
+}
+
+const postService = new Post();
+
+export default postService;
